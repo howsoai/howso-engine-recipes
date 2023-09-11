8a8967f6de2832f7c3f4861fd6368544eb53044d
Jacob Beel <54082169+jdbeel@users.noreply.github.com>
2023-09-11 16:18:43 -0400

    17336: Fix recipe tests after introduction of visuals library (#6)
    - Fixes tests for `engine_drift_monitoring` and
    `engine_react_interpret`.

--- tests/test_engine_interpret_react.py
+++ tests/test_engine_interpret_react.py
@@ -8,6 +8,7 @@ def test_predicted_mean_interval(tb):
     """
     tb.inject(
         """
+        r_val = react["action"].loc[0, 'moid']
         upper_limit = (r_val + global_residual)
         lower_limit = (r_val - global_residual)
 
@@ -45,7 +46,7 @@ def test_KDE_prediction_interval(tb):
         from scipy.integrate import quad
 
         r_val = react["action"].loc[0, 'moid']
-        lower_bound = r_val - global_residual
+        lower_bound = r_val - global_residual 
         upper_bound = r_val + global_residual
 
         from scipy.stats import gaussian_kde
