90373e632ed8a546723ae978a9c09ab6b3264fc2
jack-xia-dp <131488895+jack-xia-dp@users.noreply.github.com>
2023-09-11 09:30:57 -0400

    add ts

---
+++ tests/test_engine_timeseries.py
@@ -0,0 +1,13 @@
+tb_filename = "timeseries.ipynb"
+
+
+def test_r2(tb):
+    """Ensure good r2 given the dataset."""
+
+    tb.inject(
+        """
+        r2 = results['f1']['r2']
+        """
+    )
+
+    assert tb.ref("r2") >= 0.9
