adbb0a1ab75df8b3d34cfad733a2fd809eeefd08
Martin Koistinen <615759+mkoistinen@users.noreply.github.com>
2023-09-18 15:20:27 -0400

    17489: Add JSON-as-a-feature recipe and test (#14)
    Adds a JSON-as-a-feature recipe and test.
    
    ---------
    
    Co-authored-by: Jacob Beel <54082169+jdbeel@users.noreply.github.com>

---
+++ tests/test_engine_json_features.py
@@ -0,0 +1,13 @@
+tb_filename = "engine_json_features.ipynb"
+
+
+def test_action_length(tb):
+    """Ensure good accuracy given the dataset."""
+
+    tb.inject(
+        """
+        assert len(discriminative_action) == 1
+        assert len(generative_reaction_25) == 5
+        assert len(generative_reaction_1) == 5
+        """
+    )
