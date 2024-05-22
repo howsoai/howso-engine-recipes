01a37910c84fd0467bca8e2e983bdd0b741f9624
jack-xia-dp <144161208+jackx111@users.noreply.github.com>
2024-05-22 12:02:19 -0400

    save

--- tests/test-conditioned-insights.py
+++ tests/test-conditioned-insights.py
@@ -3,7 +3,7 @@ tb_filename = "conditioned-insights.ipynb"
 
 def test_final_output(tb):
     """
-    Test that category is not empty.
+    Test that sex_1_conditioned_accuracy is not 0.
     """
 
-    assert len(tb.ref("category")) > 0
+    assert len(tb.ref("sex_1_conditioned_accuracy")) > 0
