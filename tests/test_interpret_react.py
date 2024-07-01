148c57dcbdc597ebeb4210cdb110d830de147d39
jack-xia-dp <144161208+jackx111@users.noreply.github.com>
2024-07-01 15:58:55 -0400

    save

--- tests/test_interpret_react.py
+++ tests/test_interpret_react.py
@@ -36,32 +36,3 @@ def test_predicted_value_interval(tb):
 
     assert (tb.ref("action_value") <= tb.ref("upper_limit"))
     assert (tb.ref("action_value") >= tb.ref("lower_limit"))
-
-
-def test_KDE_prediction_interval(tb):
-    """
-    Tested the percentage area under the curve for the KDE
-    in relation to the prediction interval.
-    """
-    tb.inject(
-        """
-        from scipy.integrate import quad
-
-        r_val = react["action"].loc[0, 'moid']
-        global_residual = trainee.react_aggregate(details={"prediction_stats":True})['moid']['mae']
-        lower_bound = r_val - global_residual
-        upper_bound = r_val + global_residual
-
-        from scipy.stats import gaussian_kde
-        kde = gaussian_kde(gen_reacts)
-
-        # Define the function to integrate (the KDE)
-        def integrand(x):
-            return kde(x)
-
-        # Integrate and get area under the curve
-        result, _ = quad(integrand, lower_bound, upper_bound)
-        """
-    )
-
-    assert (tb.ref("result") >= tb.ref("0.45"))
