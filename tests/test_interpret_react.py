3e1753fcc4ef2c6dc0e093401cfa248ed96ae0c9
Cade Mack <24661281+cademack@users.noreply.github.com>
2025-02-17 11:05:32 -0500

    22875: Updates boundary_values test (#236)

--- tests/test_interpret_react.py
+++ tests/test_interpret_react.py
@@ -30,7 +30,7 @@ def test_boundary_values(tb):
     boundary_values = tb.ref("boundary_values")
 
     assert boundary_values['e'][0] is None  # no boundary below the value for e
-    assert 0.16 <= boundary_values['e'][1] <= 0.30
+    assert 0.16 <= boundary_values['e'][1] <= 0.38
 
     assert 1.50 <= boundary_values['q'][0] <= 2.00
     assert boundary_values['q'][1] is None  # no boundary above the value for q
