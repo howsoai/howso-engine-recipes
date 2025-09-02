e1efd6dab0f29cbd590612e86c2e9aeb75728f95
Jack Xia <144161208+jackx111@users.noreply.github.com>
2025-09-02 12:26:32 -0400

    24205: Get rid of get_prediction_stats (#324)
    Co-authored-by: Jacob Beel <54082169+jdbeel@users.noreply.github.com>
    Co-authored-by: howso-automation <support@howso.com>

--- tests/test_interpret_react.py
+++ tests/test_interpret_react.py
@@ -10,7 +10,7 @@ def test_predicted_mean_interval(tb):
     """
     tb.inject(
         """
-        global_residual = trainee.get_prediction_stats(details={"prediction_stats":True})['moid']['mae']
+        global_residual = trainee.react_aggregate(details={"prediction_stats":True})['moid']['mae']
         r_val = react["action"].loc[0, 'moid']
         upper_limit = (r_val + global_residual)
         lower_limit = (r_val - global_residual)
